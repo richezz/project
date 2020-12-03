908a024179818c9ee4cdc896d17e598901b1f161
Rich <example@gmail.com>
2020-12-03 20:48:27 +0300

    first

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict'; // обязательная конструкция стандарта e6
+
+
+
+
+
+let persone = {
+	name: "John",
+	age: 25,
+	isMarried: false
+};
+
+console.log(persone['name']);
+console.log(persone.age);
+console.log(persone.isMarried);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[1])
+
+// alert('Hello World!');
+
+// let answer = confirm('are u here?');
+
+// console.log(answer);
+
+// let answer = prompt('Есть ли вам 18?', 'Да');
+
+// console.log(answer);
+
+// let answer = prompt('Есть ли вам 18?', 'Да');
+
+// console.log(typeof(arr)); // с помощью typeof проверяем тип данных ( string )
+
+// console.log('arr' + ' - object');
+
+let incr = 10,
+	decr = 10;
+
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
